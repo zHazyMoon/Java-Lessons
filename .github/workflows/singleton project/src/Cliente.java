75e9a8ee27350c3defd590eae3651860e88e87f1
Victor Mello <71910690+zHazyMoon@users.noreply.github.com>
2022-02-18 11:14:28 -0300

    Add files via upload

---
+++ .github/workflows/singleton project/src/Cliente.java
@@ -0,0 +1,17 @@
+
+import java.sql.SQLException;
+import persistencia.Conexao;
+import visao.JanelaPrincipal;
+
+
+public class Cliente {
+
+  
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+   // while(Thread.currentThread().isAlive()){ 
+
+     JanelaPrincipalAction janela = new JanelaPrincipalAction();
+  //  }
+    }
+    
+}
